b0c68fbc9e5839a0d66f114890af9aca7f60794a
DenShtephen <135002535+DenShtephen@users.noreply.github.com>
2023-08-16 21:50:52 +0300

    1-4 lesson 1-1 without css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { Profile } from './Profile'
+import { Statistics } from './Statistics'
+import { FriendList } from './Friends'
+import {TransactionHistory} from './TransactionHistory'
+import user from '../data/user.json';
+import data from '../data/data.json'
+import friends from '../data/friends.json'
+import transactions from '../data/transactions.json'
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1>HOMEWORK №1</h1>
+      <Profile
+        {...user}
+      />;
+ <Statistics title="Upload stats" stats={data} />;
+      <Statistics stats={data} />;
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
+    </>
   );
 };
